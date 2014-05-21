b2c70f505c7d0ae6df8a60d474567c0ac1ce81d9
Erfan <erfanimani@gmail.com>
2014-05-21 18:10:46 +0800

    Added getLocationByState method to Location block. This allows you to group locations by state, super handy

--- Location/Block/Location.php
+++ Location/Block/Location.php
@@ -6,6 +6,20 @@ class Doghouse_Location_Block_Location extends Mage_Core_Block_Template {
 		return Mage::getModel('dhlocation/location')->getCollection();
 	}
 
+    public function getLocationsByState() {
+        $collection = $this->getLocations();
+        $groups = array();
+        foreach($collection as $item) {
+            $hash = md5(strtolower($item->getState()));
+            if(array_key_exists($hash, $groups)) {
+                array_push($groups[$hash], $item);
+            } else {
+                $groups[$hash] = array($item);
+            }
+        }
+        return $groups;
+    }
+
 	public function getHours(Doghouse_Location_Model_Location $location) {
 		return Mage::getModel('dhlocation/hour')->getCollection()
 			->addFieldToFilter('location_id', $location->getId())

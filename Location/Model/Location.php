74b076171eb910aa0ebc47fc5da8df2aaec86a5a
Rob Shepherd <rob@dhmedia.com.au>
2016-01-04 14:39:24 +0800

    Added method to getHours for a location

--- Location/Model/Location.php
+++ Location/Model/Location.php
@@ -1,10 +1,32 @@
 <?php
 
-class Doghouse_Location_Model_Location extends Mage_Core_Model_Abstract {
+class Doghouse_Location_Model_Location extends Mage_Core_Model_Abstract
+{
+    /**
+     * Store hours
+     */
+    protected $_hours;
 
     protected function _construct()
     {
         $this->_init('dhlocation/location');
+    }
+
+    /**
+     * Get opening hours for a location.
+     *
+     * @return mixed
+     */
+    public function getHours()
+    {
+
+        if (!$this->_hours) {
+            $this->_hours = Mage::getModel('dhlocation/hour')
+                ->getCollection()
+                ->addFieldToFilter('location_id', $this->getId());
+        }
+
+        return $this->_hours;
     }
 
 }
